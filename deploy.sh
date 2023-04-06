a4926a6d3388bb63f0a7759e85eb058d4722ff89
Max Thanachai Thongkum <singmax161@gmail.com>
2023-04-07 04:46:24 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,8 @@
 #git add --chmod=+x .\deploy.sh
 
-set -a
-source ./env.env
-
-echo "SERVICE_NAME=$SERVICE_NAME"
-
-docker pull $DOCKER_IMAGE
-
 mkdir -p ./data
 
 chmod -R 777 ./data
 
 echo "Deploy stack"
-docker stack deploy -c ./stack.yml $SERVICE_NAME
+docker stack deploy -c ./stack.yml simple_private_registry
